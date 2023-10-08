0d9bbaecb0806838fd61fdc731b8f132c37e4d5a
mahezarya <mahezarya23@gmail.com>
2023-10-08 20:45:15 +0700

    tugas MSIB

---
+++ JavaScript/eventHandler.js
@@ -0,0 +1,111 @@
+
+// Suara Pada Gambar Burung
+
+$(document).ready(function() {
+  // Event saat gambar pada Card 1 diklik
+  $('#gambarBurung1').click(function() {
+      var audio = document.getElementById('audioBurung1');
+      if (audio.paused) {
+          audio.play();
+      } else {
+          audio.pause();
+          audio.currentTime = 0;
+      }
+  });
+
+  // Event saat gambar pada Card 2 diklik
+  $('#gambarBurung2').click(function() {
+      var audio = document.getElementById('audioBurung2');
+      if (audio.paused) {
+          audio.play();
+      } else {
+          audio.pause();
+          audio.currentTime = 0;
+      }
+  });
+
+  // Event saat gambar pada Card 3 diklik
+  $('#gambarBurung3').click(function() {
+      var audio = document.getElementById('audioBurung3');
+      if (audio.paused) {
+          audio.play();
+      } else {
+          audio.pause();
+          audio.currentTime = 0;
+      }
+  });
+});
+
+// Suara Pada Gambar Hewan Buas
+
+$(document).ready(function() {
+  // Event saat gambar pada Card 1 diklik
+  $('#gambarHewanBuas1').click(function() {
+      var audio = document.getElementById('audioHewanBuas1');
+      if (audio.paused) {
+          audio.play();
+      } else {
+          audio.pause();
+          audio.currentTime = 0;
+      }
+  });
+
+  // Event saat gambar pada Card 2 diklik
+  $('#gambarHewanBuas2').click(function() {
+      var audio = document.getElementById('audioHewanBuas2');
+      if (audio.paused) {
+          audio.play();
+      } else {
+          audio.pause();
+          audio.currentTime = 0;
+      }
+  });
+
+  // Event saat gambar pada Card 3 diklik
+  $('#gambarHewanBuas3').click(function() {
+      var audio = document.getElementById('audioHewanBuas3');
+      if (audio.paused) {
+          audio.play();
+      } else {
+          audio.pause();
+          audio.currentTime = 0;
+      }
+  });
+});
+
+// Suara Pada Gambar Hewan Ternak
+
+$(document).ready(function() {
+  // Event saat gambar pada Card 1 diklik
+  $('#gambarHewanTernak1').click(function() {
+      var audio = document.getElementById('audioHewanTernak1');
+      if (audio.paused) {
+          audio.play();
+      } else {
+          audio.pause();
+          audio.currentTime = 0;
+      }
+  });
+
+  // Event saat gambar pada Card 2 diklik
+  $('#gambarHewanTernak2').click(function() {
+      var audio = document.getElementById('audioHewanTernak2');
+      if (audio.paused) {
+          audio.play();
+      } else {
+          audio.pause();
+          audio.currentTime = 0;
+      }
+  });
+
+  // Event saat gambar pada Card 3 diklik
+  $('#gambarHewanTernak3').click(function() {
+      var audio = document.getElementById('audioHewanTernak3');
+      if (audio.paused) {
+          audio.play();
+      } else {
+          audio.pause();
+          audio.currentTime = 0;
+      }
+  });
+});
